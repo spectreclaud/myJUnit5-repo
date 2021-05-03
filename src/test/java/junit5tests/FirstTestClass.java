0703c30aa4a6046452d540525eaad347ae5aa5c7
Rafał <spectreclaud@gmail.com>
2021-05-03 22:56:05 +0200

    Project chapter-3

--- src/test/java/junit5tests/FirstTestClass.java
+++ src/test/java/junit5tests/FirstTestClass.java
@@ -1,9 +1,29 @@
 package junit5tests;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FirstTestClass {
+
+    @BeforeAll
+    void beforeAll() {
+        System.out.println("--This is the before All method.--");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("----This is the before Each method.----");
+    }
+
+    @AfterAll
+    void afterAll() {
+        System.out.println("--This is the after All method.--");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("----This is the after Each method.----");
+    }
 
     @Test
     void firstMethod(){
