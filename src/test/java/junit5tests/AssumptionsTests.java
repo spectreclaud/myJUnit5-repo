71a8430a6358732c8c6d6cbe50fcff12a84f4cb4
Rafał <spectreclaud@gmail.com>
2021-08-10 21:35:29 +0200

    Project chapter-9 DisabledEnabled import optimize

--- src/test/java/junit5tests/AssumptionsTests.java
+++ src/test/java/junit5tests/AssumptionsTests.java
@@ -1,6 +1,5 @@
 package junit5tests;
 
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
